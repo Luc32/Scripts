4fda32b75ab6cf467055030179eea529a5336b43
Luc32 <vitor49lucas@gmail.com>
2020-07-15 14:18:04 -0300

    Recuperar dados

---
+++ RecuperarDados.py
@@ -0,0 +1,59 @@
+class lerXML:
+    def __init__(self):
+        self.x = 0;
+
+    #Se dataI e dataF for 0 ele irá buscar todos os xmls
+    def lerDados(self,diretorio,dataI,dataF):
+        pass
+    
+    def getProdutoCodigo(self):
+        pass
+
+    
+    def getProdutoDescricao(self):
+        pass
+
+    def getProdutoQuantidade(self):
+        pass
+
+    def getProdutoUnidade(self):
+        pass
+
+    def getProdutoPreco(self):
+        pass
+
+    def getProdutoICMS(self):
+        pass
+
+    def getProdutoBaseICMS(self):
+        pass
+
+
+    #Fornecedores
+
+    def getFornecedorCodigo(self):
+        pass
+
+    def getFornecedorNome(self):
+        pass
+
+    def getFornecedorCNPJ(self):
+        pass
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
